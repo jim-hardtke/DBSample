564bdfdb2f9a9c76c6f69348885b31adc4d0d254
jim-hardtke <jim.hardtke@flexagon.com>
2024-08-30 13:14:07 -0500

    Update create_tb1.sql

--- liquibase/DBT01/system/create_tb1.sql
+++ liquibase/DBT01/system/create_tb1.sql
@@ -1,5 +1,2 @@
---liquibase formatted sql
-
---changeset jim.hardtke:2
 CREATE TABLE test_table2 (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id));
 ALTER TABLE TEST_TABLE ADD (C1 VARCHAR2(20) );
